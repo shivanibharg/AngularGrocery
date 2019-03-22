b285fbfe6800696fcbcb8f475a9922e1573c5c60
shivanibharg <shivanibharg@gmail.com>
2019-03-22 14:54:16 -0700

    My inital commit

---
+++ src/app/app-grocery/app-grocery.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-grocery',
+  templateUrl: './app-grocery.component.html',
+  styleUrls: ['../../assets/css/bootstrap.min.css']
+})
+export class AppGroceryComponent {
+
+  task: string;
+  tasks = [];
+ 
+  onClick(){
+    if(this.task!=''){
+    this.tasks.push({name: this.task});
+    this.task = '';
+    }
+    else{ {"Error"} }
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+//import { AppComponent } from './app.component';
+import { AppGroceryComponent } from './app-grocery/app-grocery.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    
+    AppGroceryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppGroceryComponent]
 })
 export class AppModule { }
